e0750b8643889a685aa9574b210e8a46bae35c59
Yuta Ushijima <register@yuta-u.com>
2021-12-26 15:23:18 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ export default function App() {
 
   const onClickOpen = () => setOpen(!open);
   const onChangeText = (e) => setText(e.target.value);
+  const onClickClose = () => setOpen(false);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
