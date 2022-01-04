fb34c82d7eb399737df528a86c93c2c252f63d6f
Yuta Ushijima <register@yuta-u.com>
2022-01-04 19:10:11 +0900

    initial commit

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 import { Home } from "../Home";
-import { Page2 } from "../Page2";
 import { page1Routes } from "./Page1Routes";
+import { page2Routes } from "./Page2Routes";
 
 export const Router = () => {
   return (
@@ -25,9 +25,22 @@ export const Router = () => {
           </Switch>
         )}
       ></Route>
-      <Route path="/page2">
-        <Page2 />
-      </Route>
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page2Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      ></Route>
     </Switch>
   );
 };
