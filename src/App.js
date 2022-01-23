38c06f761b10c63c7013476c7b909a8404eaf04d
Yuta Ushijima <register@yuta-u.com>
2022-01-23 15:53:06 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard />
     </div>
   );
 }
