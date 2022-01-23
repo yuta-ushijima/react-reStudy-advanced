3b1c06d03e7ea4c8c427d312a8c8d363e611f5bc
Yuta Ushijima <register@yuta-u.com>
2022-01-23 18:07:05 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "Jack",
-  image: "https://source.unsplash.com/JaC_kHyhO7k",
-  email: "1234@example.com",
-  phone: "090-1111-2222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `Jack-${val}`,
+    image: "https://source.unsplash.com/JaC_kHyhO7k",
+    email: "1234@example.com",
+    phone: "090-1111-2222",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://google.com"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <SUserArea>
+        <h2>ユーザー一覧</h2>
+        <SearchInput />
+        {users.map((user) => (
+          <UserCard keys={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
